c0e2933a028c7cda1b33f4950a5a85fd521b8aa3
Syed Thaseemuddin <syed.thaseemuddin@microchip.com>
2025-01-29 09:23:11 +0530

    [APPS_DOC] Added TM dev_files M32MM-2240

---
+++ apps/pic32cm_mc00_cnano/pic32cmmc_smart_appliance_control/dev_files/pic32cm_mc00_cnano/main.c
@@ -0,0 +1,86 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*
+    (c) 2021 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+// DOM-IGNORE-END
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include <stdio.h>
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include <string.h>
+#include "definitions.h"                // SYS function prototypes
+#include "click_routines/click_interface.h"
+#include "click_routines/weather/weather.h"
+#include "click_routines/fan/fan.h"
+#include "click_routines/eink_bundle/eink_bundle.h"
+#include "click_routines/eink_bundle/eink_bundle_image.h"
+#include "click_routines/eink_bundle/eink_bundle_font.h"
+#include "ble/ble.h"
+
+char BLE_Cmd_buf[100];
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    int16_t temperature = 0;
+    int16_t dummy_temperature = 0;
+    char  buffer1[10], buffer[10];
+    char temp_buffer[10];
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
---
+++ apps/pic32cm_mc00_curiosity_pro/pic32cm_mc_curiosity_getting_started/dev_files/pic32cm_mc00_curiosity_pro/main.c
@@ -0,0 +1,100 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+/*******************************************************************************
+* Copyright (C) 2020 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+
+#define TEMP_SENSOR_SLAVE_ADDR                         0x004F
+#define TEMP_SENSOR_REG_ADDR                           0x00
+
+static volatile bool isTemperatureRead = false;
+static uint8_t temperatureVal;
+static uint8_t i2cWrData = TEMP_SENSOR_REG_ADDR;
+static uint8_t i2cRdData[2] = {0};
+static uint16_t adcResult;
+
+static uint8_t getTemperature(uint8_t* rawTempValue)
+{
+    int16_t temp;
+    // Convert the temperature value read from sensor to readable format (Degree Celsius)
+    // For demonstration purpose, temperature value is assumed to be positive.
+    // The maximum positive temperature measured by sensor is +125 C
+    temp = (rawTempValue[0] << 8) | rawTempValue[1];
+    temp = (temp >> 7) * 0.5;
+    temp = (temp * 9/5) + 32;
+    return (uint8_t)temp;
+}
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+ 
+    while ( true )
+    {
+      //user code
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
