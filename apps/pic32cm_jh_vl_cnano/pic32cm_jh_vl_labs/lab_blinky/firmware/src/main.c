799b49ca513c74f64620b7faab2ddd930793ca6e
Robert Perkel <robert.perkel@microchip.com>
2025-06-10 23:40:34 -0700

    Adjusted include file | M32MM-3186

--- apps/pic32cm_jh_vl_cnano/pic32cm_jh_vl_labs/lab_blinky/firmware/src/main.c
+++ apps/pic32cm_jh_vl_cnano/pic32cm_jh_vl_labs/lab_blinky/firmware/src/main.c
@@ -25,7 +25,7 @@
 #include <stddef.h>                     // Defines NULL
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h>                     // Defines EXIT_FAILURE
-#include "definitions.h"                // SYS function prototypes
+#include <definitions.h>                // SYS function prototypes
 
 
 // *****************************************************************************
