9c89dd3231016fb669b4d2388a0495a86d4b7bfd
Sountharya Paranthaman <sountharya.paranthaman@microchip.com>
2025-09-23 12:15:20 +0530

    [APPS_DOCS] Internal: SHD based Can MCP25625 demo on PIC32CM JH VL Cnano | M32MM-3244

---
+++ apps/pic32cm_jh_vl_cnano/pic32cm_jh_vl_cnano_mikroe_click/mcp25625/firmware/src/click_routines/click_interface.h
@@ -0,0 +1,68 @@
+/*******************************************************************************
+  Click interface header file
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    click_interface.h
+
+  Summary:
+    MCP25625 click routine interface header file.
+
+  Description
+    This file defines the interface to the MCP25625 click routines and MHC
+    PLIB APIs.
+
+  Remarks:
+    None.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+#ifndef _CLICK_INTERFACE_H
+#define _CLICK_INTERFACE_H
+
+/**
+  Section: Included Files
+ */
+
+/** Click routine interfaces/resources Macro Declarations
+ *
+ *  1. SPI
+ *  The MCP25625 click example on SAM E51 Curiosity Nano uses
+ *  mikroBUS socket #1 on the Curiosity Nano Base for Click boards
+ *  to mount MCP25625 click board. The SPI lines from MCU coming to
+ *  this socket are from SERCOM1 peripheral on the MCU.
+ *
+ */
+
+// SERCOM SPI Definitions
+#define CLICK_MCP25625_Read                          SERCOM0_SPI_Read
+#define CLICK_MCP25625_Write                         SERCOM0_SPI_Write
+
+#endif // _CLICK_INTERFACE_H
---
+++ apps/pic32cm_jh_vl_cnano/pic32cm_jh_vl_cnano_mikroe_click/mcp25625/readme.md
@@ -0,0 +1,5 @@
+# SHD based Click board (CAN-mcp25625) example on PIC32CM JH VL Curiosity Nano Evaluation Kit + Nano Explorer
+
+-------
+
+[Click Here](https://onlinedocs.microchip.com/v2/keyword-lookup?keyword=PIC32CM_JH_VL_SHD_MCP25625&redirect=true) to view documentation
